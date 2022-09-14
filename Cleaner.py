612aa35bc405d03de6a9ee9801a6177548924f66
YaroslavZq <svyatikua2@gmail.com>
2022-09-14 18:06:54 +0300

    def cleaner: у гілках іф позабирав непотрібні "is True"
    def cleaner та def normalize виніс на початок перед def main
    
    створено def create_directory для створень сортувальних папок, виправлено деякі помилки

--- Cleaner.py
+++ Cleaner.py
@@ -22,32 +22,48 @@ def normalize(dir_file):
     return new_file_name
 
 
+# створення папок для сортування
+def create_directory(path, name):
+    directory = os.path.join(path, name)
+    try:
+        os.mkdir(directory)
+    except FileExistsError:
+        print(f"{name} already created")
+    return directory
+
+
 # функція перебору файлів по папці
-def cleaner(path_holder):
+def cleaner(path_holder, path):
     print(path_holder)
     for file in path_holder.iterdir():
         print(file)
         if file.is_dir() and file.name not in DIR_IGNOR_NAMES:
-            cleaner(file)
+            cleaner(file, path)
         elif file.is_dir() and file.name in DIR_IGNOR_NAMES:
             continue
         new_file_name = Path(normalize(file))
         if new_file_name.is_file() and new_file_name.suffix in ['.jpeg', '.png', '.jpg', '.svg']:
+            dir_img = create_directory(path, "images")
             shutil.move(new_file_name, dir_img)
         elif new_file_name.is_file() and new_file_name.suffix in ['.avi', '.mp4', '.mov', '.mkv']:
+            dir_vid = create_directory(path, "video")
             shutil.move(new_file_name, dir_vid)
         elif new_file_name.is_file() and new_file_name.suffix in ['.doc', '.docx', '.txt', '.pdf', '.xlsx',
                                                                           '.xls', '.pptx']:
+            dir_doc = create_directory(path, "documents")
             shutil.move(new_file_name, dir_doc)
         elif new_file_name.is_file() and new_file_name.suffix in ['.mp3', '.ogg', '.wav', '.amr']:
+            dir_aud = create_directory(path, "audio")
             shutil.move(new_file_name, dir_aud)
         elif new_file_name.is_file() and new_file_name.suffix in ['.zip', '.gz', '.tar']:
+            dir_arc = create_directory(path, "archives")
             archive = Path(shutil.move(new_file_name, dir_arc))
             new_folder = os.path.join(archive.parent, archive.name.split(".")[0])
             os.mkdir(new_folder)
             shutil.unpack_archive(archive, new_folder)
             os.remove(archive)
         elif new_file_name.is_file():
+            dir_other = create_directory(path, "other")
             shutil.move(new_file_name, dir_other)
     # видалення порожніх та непотрібних папок
     for file in path_holder.iterdir():
@@ -63,40 +79,7 @@ def main():
     if not (os.path.exists(path) and Path(path).is_dir()):
         print("Path incorrect")
         exit()
-
-    # створення папок для сортування
-    try:
-        dir_img = (os.path.join(path, "images"))
-        os.mkdir(dir_img)
-    except FileExistsError:
-        print("images already created")
-    try:
-        dir_vid = (os.path.join(path, "video"))
-        os.mkdir(dir_vid)
-    except FileExistsError:
-        print("video already created")
-    try:
-        dir_doc = (os.path.join(path, "documents"))
-        os.mkdir(dir_doc)
-    except FileExistsError:
-        print("documents already created")
-    try:
-        dir_aud = (os.path.join(path, "audio"))
-        os.mkdir(dir_aud)
-    except FileExistsError:
-        print("audio already created")
-    try:
-        dir_arc = (os.path.join(path, "archives"))
-        os.mkdir(dir_arc)
-    except FileExistsError:
-        print("archives already created")
-    try:
-        dir_other = (os.path.join(path, "other"))
-        os.mkdir(dir_other)
-    except FileExistsError:
-        print("other already created")
-
-    cleaner(Path(path))
+    cleaner(Path(path), path)
 
 
 if __name__ == "__main__":
